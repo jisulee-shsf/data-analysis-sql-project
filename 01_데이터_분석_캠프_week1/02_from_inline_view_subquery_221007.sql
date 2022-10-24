de5a2dc84bd9083c18419356759f046d2d40d584
Jisu Lee <jisulee.shsf@gmail.com>
2022-10-25 06:54:45 +0900

    Upload file

---
+++ 01_데이터_분석_캠프_week1/02_from_inline_view_subquery_221007.sql
@@ -0,0 +1,36 @@
+# products에서 category_id별 price가 10월 매출액에서 차지하는 비율 출력하기 
+# category_id / oct_sales / price / price_pct 출력
+# price_pct는 높은 순으로 정렬하여 상위 30개 값만 출력
+
+-- 1-1. FROM절 서브쿼리
+SELECT table1.category_id
+     , table1.oct_sales
+     , table2.price
+     , table2.price * 100 / table1.oct_sales AS price_pct
+FROM (SELECT category_id
+           , SUM(price) AS oct_sales
+	  FROM products
+      WHERE order_timestamp >= '2021-10-01'
+		AND order_timestamp < '2021-11-01'
+      GROUP BY 1) AS table1
+INNER JOIN products table2 ON table1.category_id = table2.category_id
+ORDER BY 4 DESC
+LIMIT 30;
+
+-- 1-2. FROM절 서브쿼리 - WITH문 사용
+WITH table1 AS (
+SELECT category_id
+     , SUM(price) AS oct_sales
+FROM products
+WHERE order_timestamp >= '2021-10-01'
+  AND order_timestamp < '2021-11-01'
+GROUP BY 1)
+
+SELECT table1.category_id
+     , table1.oct_sales
+     , table2.price
+     , table2.price * 100 / table1.oct_sales AS price_pct
+FROM table1
+INNER JOIN products table2 ON table1.category_id = table2.category_id
+ORDER BY 4 DESC
+LIMIT 30;
