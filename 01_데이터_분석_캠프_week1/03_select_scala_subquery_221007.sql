7ee862a65daf9d25bb998f2b1a2dbf4c39065951
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-25 14:11:07 +0900

    Update 03_select_scala_subquery_221007.sql

--- 01_데이터_분석_캠프_week1/03_select_scala_subquery_221007.sql
+++ 01_데이터_분석_캠프_week1/03_select_scala_subquery_221007.sql
@@ -1,14 +1,14 @@
 -- 1-1. SELECT절 서브쿼리
 # products에서 price가 category_id별 전체 매출액에서 차지하는 비율 출력하기 
 # category_id / sales / price / price_pct 출력
-# price_pct가 높은 순으로 정렬한 후, 상위 30개 값만 출력
+# price_pct가 높은 순으로 정렬한 후, 상위 11번째에서 20번째 값 출력하기
 select table1.category_id
      , table1.price
      , (select SUM(price) from products table2 where table1.category_id = table2.category_id) AS sales
      , table1.price * 100 / (select SUM(price) from products table2 where table1.category_id = table2.category_id) AS price_pct
 from products table1
-ORDER BY 3 DESC
-LIMIT 30;
+ORDER BY 4 DESC
+LIMIT 10 OFFSET 10;
 
 -- 1-2. SELECT절 서브쿼리
 # products에서 price가 전체 매출액에서 차지하는 비율 출력하기 
@@ -19,5 +19,5 @@ select table1.category_id
      , (select SUM(price) from products table2) AS sales
      , table1.price * 100 / (select SUM(price) from products table2) AS price_pct
 from products table1
-ORDER BY 3 DESC
-LIMIT 30;
+ORDER BY 4 DESC
+LIMIT 10 OFFSET 10;
