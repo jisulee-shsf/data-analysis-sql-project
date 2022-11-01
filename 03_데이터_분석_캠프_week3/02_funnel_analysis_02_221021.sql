5f7be82e55b1f3e1ecc4ff2a5c6266b3b949ea16
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-11-01 09:18:49 +0900

    Update 02_funnel_analysis_02_221021.sql

--- 03_데이터_분석_캠프_week3/02_funnel_analysis_02_221021.sql
+++ 03_데이터_분석_캠프_week3/02_funnel_analysis_02_221021.sql
@@ -1,3 +1,8 @@
+-- 기초반 웹사이트 유입 채널별 view / scroll / click 세션 수와 view → scroll / scroll → click / view → click 전환율 출력하기 
+# source & medium 정보는 page_location 컬럼 내에서 추출
+# 각 이벤트 간의 선후관계를 고려하고, 동시에 일어난 이벤트도 전환 카운팅에 포함
+# view → click 전환율을 기준으로 내림차순 정렬하나, 전환율이 동일할 경우 view 세션 수를 기준으로 내림차순 정렬
+
 WITH page_view AS (
 SELECT user_id
      , session_id
@@ -18,7 +23,7 @@ SELECT user_id
      , event_timestamp
      , page_location
 FROM ga_report
-WHERE page_title LIKE '%기초반' AND event_name LIKE 'SQL%'
+WHERE page_title LIKE '%기초반' AND event_name LIKE 'click%'
 )
 
 SELECT CASE WHEN page_view.page_location LIKE '%source%' THEN SUBSTRING_INDEX(SUBSTRING_INDEX(page_view.page_location, '&', 1), '=', -1) ELSE '(direct)' END AS source
@@ -34,10 +39,10 @@ SELECT CASE WHEN page_view.page_location LIKE '%source%' THEN SUBSTRING_INDEX(SU
      / COUNT(DISTINCT page_view.user_id, page_view.session_id), 2) AS view_click_rate
 FROM page_view 
 LEFT JOIN page_scroll ON page_view.user_id = page_scroll.user_id
-                 AND page_view.session_id = page_scroll.session_id
-                 AND page_view.event_timestamp <= page_scroll.event_timestamp
+                     AND page_view.session_id = page_scroll.session_id
+                     AND page_view.event_timestamp <= page_scroll.event_timestamp
 LEFT JOIN page_click ON page_scroll.user_id = page_click.user_id
-                 AND page_scroll.session_id = page_click.session_id
-                 AND page_scroll.event_timestamp <= page_click.event_timestamp
+                    AND page_scroll.session_id = page_click.session_id
+                    AND page_scroll.event_timestamp <= page_click.event_timestamp
 GROUP BY 1, 2
 ORDER BY view_click_rate DESC, view_session_cnt DESC;
