ab50b4839e84370febbb50254c1181cd9e0ba565
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-11-02 18:19:15 +0900

    Update 01_window_function_01_221028.sql

--- 04_데이터_분석_캠프_week4/01_window_function_01_221028.sql
+++ 04_데이터_분석_캠프_week4/01_window_function_01_221028.sql
@@ -26,10 +26,11 @@ SELECT category
      , ROUND(SUM(sales_sub_category) OVER (), 2) AS sales_total
      , ROUND(sales_sub_category / SUM(sales_sub_category) OVER (PARTITION BY category) * 100, 2) AS pct_in_category 
      , ROUND(sales_sub_category / SUM(sales_sub_category) OVER () * 100, 2) AS pct_in_total
-FROM (SELECT category
-	   , sub_category
-           , ROUND(SUM(sales), 2) AS sales_sub_category
-      FROM records
-      GROUP BY 1, 2) records
+FROM (
+     SELECT category
+	  , sub_category
+          , ROUND(SUM(sales), 2) AS sales_sub_category
+     FROM records
+     GROUP BY 1, 2) records
 GROUP BY 1, 2
 ORDER BY pct_in_total DESC;
